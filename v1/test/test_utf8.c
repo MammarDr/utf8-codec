ec7b8f90668604e4eaae23c726386c847a3afb88
MammarDr <140546722+MammarDr@users.noreply.github.com>
2026-05-01 19:48:23 +0100

    Update test_utf8.c

--- v1/test/test_utf8.c
+++ v1/test/test_utf8.c
@@ -36,10 +36,60 @@ void test_decode(void)
  
 }
 
+void test_encode(void)
+{
+    uint8_t* result;
+    const char* str1[] = { "00041", NULL };
+    result = utf8_encode((char**)str1);
+    assert(result[0] == 0x41);
+    assert(result[1] == 0x00);
+    free(result);
+
+    const char* str2[] = { "000E9", NULL }; // é
+    result = utf8_encode((char**)str2);
+    assert(result[0] == 0xC3);
+    assert(result[1] == 0xA9);
+    assert(result[2] == 0x00);
+    free(result);
+
+    const char* str3[] = { "020AC", NULL }; // €
+    result = utf8_encode((char**)str3);
+    assert(result[0] == 0xE2);
+    assert(result[1] == 0x82);
+    assert(result[2] == 0xAC);
+    assert(result[3] == 0x00);
+    free(result);
+
+    const char* str4[] = { "1F609", NULL }; // 😉
+    result = utf8_encode((char**)str4);
+    assert(result[0] == 0xF0);
+    assert(result[1] == 0x9F);
+    assert(result[2] == 0x98);
+    assert(result[3] == 0x89);
+    assert(result[4] == 0x00);
+    free(result);
+
+    const char* str5[] = { "00041", "00633", "000E9", "1F609", NULL }; // Aسé😉
+    result = utf8_encode((char**)str5);
+    assert(result[0] == 0x41);
+    assert(result[1] == 0xD8);
+    assert(result[2] == 0xB3);
+    assert(result[3] == 0xC3);
+    assert(result[4] == 0xA9);
+    assert(result[5] == 0xF0);
+    assert(result[6] == 0x9F);
+    assert(result[7] == 0x98);
+    assert(result[8] == 0x89);
+    assert(result[9] == 0x00);
+    free(result);
+ 
+}
+
 int main(void)
 {
     test_utf8_char_length();
     test_decode();
+    test_encode();
     printf("All UTF-8 tests passed.\n");
     return 0;
 }
